d25c54eb4e8ad5fd0782bef5f5498c4c912aa351
Ahmet Salih Seçkin <36454018+seckinsalihDev@users.noreply.github.com>
2021-05-05 19:42:34 +0300

    hata düzeltmesi

--- lib/tabsayfalari/ilandetay.dart
+++ lib/tabsayfalari/ilandetay.dart
@@ -85,7 +85,7 @@ class _IlanDetayState extends State<IlanDetay> {
                   borderRadius: BorderRadius.circular(12)),
               child: Padding(
                 padding: const EdgeInsets.all(8.0),
-                child: Text("a"),
+                child: Text("Aranan Yetenekler"),
               ),
             ),
             SizedBox(
@@ -99,7 +99,7 @@ class _IlanDetayState extends State<IlanDetay> {
                   borderRadius: BorderRadius.circular(12)),
               child: Padding(
                 padding: const EdgeInsets.all(8.0),
-                child: Text("b"),
+                child: Text("Proje Açıklaması"),
               ),
             ),
             Container(
@@ -114,54 +114,6 @@ class _IlanDetayState extends State<IlanDetay> {
                 ],
               ),
             )
-
-            Row(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(14.0),
-                  child: Text("22/06/2020"),
-                )
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                SizedBox(),
-                SizedBox(),
-                SizedBox(),
-                SizedBox(),
-                SizedBox(),
-                ElevatedButton(
-                  onPressed: () {},
-                  child: Text("Başvuru yap"),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: height * 3 / 100,
-            ),
-            Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-              Text("İlan Başlığı"),
-              SizedBox(
-                width: width * 4 / 100,
-              ),
-              SizedBox(),
-              ElevatedButton(
-                onPressed: () {},
-                child: Text("Favoriye ekle"),
-              ),
-            ]),
-            Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-              Text("Aranan yetenekler"),
-              SizedBox(
-                width: width * 4 / 100,
-              ),
-              SizedBox(),
-              ElevatedButton(
-                onPressed: () {},
-                child: Text("Mesaj gönder"),
-              ),
-            ]),
           ],
         ),
       ),
